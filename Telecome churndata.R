7743f86584cb4f7ab7e1d6953ade0819af64424c
DevOpsInaBox <DevOpsInaBox@newtglobal.com>
2017-08-07 09:29:29 +0530

    checkin

--- Telecome churndata.R
+++ Telecome churndata.R
@@ -15,7 +15,6 @@ library(rattle)
 
 #setwd("E:\\Data Creation\\Telecom")
 telecomdata<-read.csv("WA_Fn-UseC_-Telco-Customer-Churn.csv",header = T)
-head(telecomdata)
 which(is.na(telecomdata$tenure))
 telecomdata[!complete.cases(telecomdata), ]
 
@@ -46,8 +45,10 @@ telecomdata$tenure_interval <- as.factor(telecomdata$tenure_interval)
 
 # Ignore the variables with more levels while predicting the model
 # Columns "customerID" and "tenure" having more levels
-
 telecomdata <- select(telecomdata,-customerID,-tenure)
+
+
+# convert factor variables into character variables before changing the values
 telecomdata$MultipleLines <- as.character(telecomdata$MultipleLines)
 telecomdata$OnlineSecurity <- as.character(telecomdata$OnlineSecurity)
 telecomdata$OnlineBackup <- as.character(telecomdata$OnlineBackup)
@@ -57,7 +58,6 @@ telecomdata$StreamingTV <- as.character(telecomdata$StreamingTV)
 telecomdata$StreamingMovies <- as.character(telecomdata$StreamingMovies)
 
 
-# convert factor variables into character variables before changing the values
 telecomdata$MultipleLines[telecomdata$MultipleLines=="No phone service"] <- "No"
 telecomdata$OnlineSecurity[telecomdata$OnlineSecurity=="No internet service"] <- "No"
 telecomdata$OnlineBackup[telecomdata$OnlineBackup=="No internet service"] <- "No"
@@ -92,90 +92,61 @@ telecomModelstep <- step(glm(Churn ~ .,family=binomial(link="logit"),data=trainD
 telecomModelstep_1<-glm(Churn ~ PaymentMethod+OnlineSecurity+MonthlyCharges+StreamingMovies+PaperlessBilling+StreamingTV+InternetService+Contract+tenure_interval+MultipleLines+SeniorCitizen,data=trainData,family=binomial(link="logit"))
 plot(telecomModelstep_1)
 print(summary(telecomModelstep_1))
+
 # test the model with test dataset
 test.predictionsstep_response <- predict(telecomModelstep_1,newdata=testData,type="response")
-test.predictionsstep_link <- predict(telecomModelstep_1,newdata=testData,type="link")
+
 # if the prediction probability is greater than 0.5 then those 
 # customers are classified as churned customer less than 0.5 are classified as not churning customer
 fitted.results <- ifelse(test.predictionsstep_response > 0.5,1,0)
 testData$Churn <- as.character(testData$Churn)
 testData$Churn[testData$Churn=="No"] <- "0"
 testData$Churn[testData$Churn=="Yes"] <- "1"
+
+# confusion matrix
+print("Confusion Matrix for Actual Churn(Y axis) and Predicted Churn(X axis)")
+table(testData$Churn,test.predictionsstep_response > 0.5)
 # calculating the misclassfication rate
 misClasificationError <- mean(fitted.results!=testData$Churn)
-print(misClasificationError)
+print(paste0("Misclassification rate for the Prediction Model is found to be : ",misClasificationError))
 # calculating the accuracy rate
 accuracyRate <- 1-misClasificationError
-print(accuracyRate)
-# confusion matrix
-table(testData$Churn,test.predictionsstep_response > 0.5)
-(1392+294)/nrow(testData)
+print(paste0("Hence, Accuracy rate for the Prediction Model is found to be : ",accuracyRate))
 
-score_data <- data.frame(link=test.predictionsstep_link, 
-                         response=test.predictionsstep_response,
-                         churn_test=testData$Churn,
-                         stringsAsFactors=FALSE)
+plot(roc(testData$Churn, test.predictionsstep_response, direction="<"), col="yellow", lwd=3, main="ROC Curve")
 
-score_data %>% 
-  ggplot(aes(x=link, y=response, col=churn_test)) + 
-  scale_color_manual(values=c("black", "red")) + 
-  geom_point() + 
-  geom_rug() + 
-  ggtitle("Both link and response scores put cases in the same order")
-
-plot(roc(testData$Churn, test.predictionsstep_response, direction="<"),
-     col="yellow", lwd=3, main="ROC Curve")
-# cbinding actual results with the predicted results
-results <- cbind(fitted.results,testData$Churn)
-colnames(results) <- c("predicted","actual")
-results <- as.data.frame(results)
-print(results)
-
-# logistic regression model on  without step on training the data
-tele<-glm(Churn ~PaymentMethod+OnlineSecurity+MonthlyCharges+StreamingMovies+PaperlessBilling+StreamingTV+InternetService+Contract+tenure_interval+MultipleLines+SeniorCitizen,family=binomial(link="logit"),data=telecomdata)
-preddicted_val<-predict(tele,type = "response")
-table(telecomdata$Churn,preddicted_val>0.5)
 #Actual ROC Curve
 prediction_object<-prediction(preddicted_val,telecomdata$Churn)
 perf_1<-performance(prediction_object,measure = "tpr",x.measure = "fpr")
 plot(perf_1)
-print(summary(tele))
 
 #Random Forest with selection variables
 set.seed(415)
 mytree_sel<-randomForest(Churn ~ PaymentMethod+OnlineSecurity+MonthlyCharges+StreamingMovies+PaperlessBilling+StreamingTV+InternetService+Contract+tenure_interval+MultipleLines+SeniorCitizen,data=trainData,importance = T)
+print("**********************************************Summary of Random Forest Prediction***************************************************")
 summary(mytree_sel)
+print("************************************************************************************************************************************")
 varImpPlot(mytree_sel)
 print(mytree_sel)
 pred_sel<-predict(mytree_sel, newdata =testData)
+print("Confusion Matrix for Random Forest Prediction")
 table(pred_sel,testData$Churn)
-(1353 + 300)/nrow(testData)
+print(paste0("Accuracy of Random Forerst Prediction:",sum(diag(table(pred_sel,testData$Churn)))/nrow(testData)))
+
+#CForest with selection variables
 mytree_sel_cf<-cforest(Churn ~ PaymentMethod+OnlineSecurity+MonthlyCharges+StreamingMovies+PaperlessBilling+StreamingTV+InternetService+Contract+tenure_interval+MultipleLines+SeniorCitizen,data=trainData,controls=cforest_unbiased(ntree=200, mtry=3))
+print("**********************************************Summary of CForest Prediction***************************************************")
+summary(mytree_sel_cf)
+print("************************************************************************************************************************************")
 Prediction_c <- predict(mytree_sel_cf, testData, OOB=TRUE, type = "response")
+print("Confusion Matrix for CForest Prediction")
 table(Prediction_c,testData$Churn)
-(1359+306)/nrow(testData)
+print(paste0("Accuracy of CForerst Prediction:",sum(diag(table(Prediction_c,testData$Churn)))/nrow(testData)))
+
 #Random Forest Plotting
-mytree_sel<-rpart(Churn ~ PaymentMethod+OnlineSecurity+MonthlyCharges+StreamingMovies+PaperlessBilling+StreamingTV+InternetService+Contract+tenure_interval+MultipleLines+SeniorCitizen,data=trainData,method ="class")
 mytree_sel<-rpart(Churn ~ PaymentMethod+OnlineSecurity+MonthlyCharges+StreamingMovies+PaperlessBilling+StreamingTV+InternetService+Contract+tenure_interval+MultipleLines+SeniorCitizen,data=trainData,method ="class",control=rpart.control(minsplit=2, cp=0.005))
-new.fit <- prp(mytree_sel,snip=TRUE)$obj
 fancyRpartPlot(mytree_sel)
 mytree<-rpart(Churn ~ .,data=trainData,method = "class")
 plot(mytree)
 text(mytree)
 fancyRpartPlot(mytree)
-#Random Forest
-set.seed(415)
-mytree<-randomForest(Churn ~ .,data=trainData,importance = T)
-summary(mytree)
-varImpPlot(mytree)
-print(mytree)
-pred<-predict(mytree, newdata =testData)
-submit<-data.frame(Churn=testData$Churn,Churn = pred)
-write.csv(submit,file = "firstforest.csv",row.names = F)
-table(pred,testData$Churn)
-(1387 + 277)/nrow(testData)
-set.seed(415)
-mytree1<-cforest(Churn ~ .,data=trainData,controls=cforest_unbiased(ntree=200, mtry=3))
-Prediction <- predict(mytree1, testData, OOB=TRUE, type = "response")
-table(Prediction,testData$Churn)
-(1397+288)/nrow(testData)
